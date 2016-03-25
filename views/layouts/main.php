6b47a691db9e0620bde3e02dd134a95f0ec44c3a
jepster <4004ar@gmail.com>
2016-03-25 23:19:16 +0100

    Switched config back to prod, removed copyright from footer.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -56,7 +56,6 @@ AppAsset::register($this);
 
     <footer class="footer">
         <div class="container">
-            <p class="pull-left">&copy; Intellipass <?= date('Y') ?></p>
             <p class="pull-right"><?= Yii::powered() ?></p>
         </div>
     </footer>
