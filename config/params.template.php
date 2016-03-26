96decd9d8cef8e4ffb2c2b5be722307a943daa55
jepster <4004ar@gmail.com>
2016-03-26 02:29:55 +0100

    Improved params template.

--- config/params.template.php
+++ config/params.template.php
@@ -3,4 +3,10 @@
 return [
     'adminEmail' => 'admin@example.com',
     'system_email' => 'system@example.com',
+    /**
+     * If single user mode is on, it's recommended to set
+     * a .htaccess and .htpasswd file for password protection.
+     */
+    'single_user_mode' => FALSE,
+    'team_secret'      => 'SET SOME TEAM SECRET'
 ];
